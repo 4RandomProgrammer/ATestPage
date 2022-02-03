bd407896c3ec99a2e3216e5f178d0f70cd51ed57
ARandomProgrammer <luis.henriques@estudante.ufscar.br>
2022-02-03 15:44:48 -0300

    script changes

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const toTop = document.querySelector(".to-top");
 
+window.addEventListener("scroll",()=>{
+  if(pageYOffset > 100){
+    toTop.classList.add("active");
+  }
+  else {
+    toTop.classList.remove("active");
+  }
+});
